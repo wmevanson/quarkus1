9fc2e828d8272f40dfe64f12438fcb637605adea
wmevanson <wmevanson@gmail.com>
2020-03-29 23:13:39 +0000

    quarkus mutiny postgres extensions

---
+++ src/main/kotlin/org/wevanson/resource/RequestResource.kt
@@ -0,0 +1,14 @@
+package org.wevanson.resouce
+
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Path("/requests")
+class RequestResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    fun hello() = "a request"
+}
